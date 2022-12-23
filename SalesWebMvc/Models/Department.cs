553025f7fe750ce5c0545a3b4887520993635b14
Luksbell <belchior1100@gmail.com>
2022-12-23 01:41:21 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        /*
+        public List<Seller> Sellers = new List<Seller>();
+
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return 2.0;
+        }
+        */
+    }
+}
