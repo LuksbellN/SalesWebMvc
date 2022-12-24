1b07b50fcf1edf1c3834c550a8b869361b8948a6
Luksbell <belchior1100@gmail.com>
2022-12-23 21:45:36 -0300

    Fixing Departments Title view

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -10,6 +10,7 @@ namespace SalesWebMvc.Models
 
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
